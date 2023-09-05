9007021dadf5a30c221d53009381677b8921de63
busracncn <busra.cann98@gmail.com>
2023-09-05 23:11:41 +0300

    rename app

--- djangofun/urls.py
+++ djangofun/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("course/", include("polls.urls")),
+    path("hogwarts/", include("hogwarts.urls")),
 ]
--- hogwarts/apps.py
+++ hogwarts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CourseConfig(AppConfig):
+class HogwartsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'course'
+    name = 'hogwarts'
