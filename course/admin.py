0fa5c833451a631bbfdd6ef9da8d3590f0cbeedd
busracncn <busra.cann98@gmail.com>
2023-09-05 21:37:35 +0300

    added models and registered models in admin

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Profession, Student, Professor, House
 
 # Register your models here.
+
+@admin.register(Profession)
+class ProfessionAdmin(admin.ModelAdmin):
+    list_display =  ['id', 'title']
+    list_filter = ['title',]
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display =  ['id', 'first_name', 'last_name']
+    list_filter = ['first_name','last_name']
+
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display =  ['id', 'first_name', 'last_name', 'profession', 'is_headmaster']
+    list_filter = ['first_name', 'last_name', 'profession']
+
+
+@admin.register(House)
+class CourseAdmin(admin.ModelAdmin):
+    list_display =  ['id', 'name', 'head']
+
--- course/models.py
+++ course/models.py
@@ -1,3 +1,38 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+
+class Profession(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Professor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    profession = models.ForeignKey(Profession, on_delete=models.CASCADE, blank=True, null=True)
+    is_headmaster = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student)
+    head = models.ForeignKey(Professor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+    
